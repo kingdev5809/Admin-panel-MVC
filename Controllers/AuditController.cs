ac30fe86db5b3570f565a131e8f2ae009bb1fc2d
KING <kingdev5809@gmail.com>
2023-10-18 13:26:35 +0500

    audit complate

---
+++ Controllers/AuditController.cs
@@ -0,0 +1,30 @@
+﻿using CrudMVCByKING.Data;
+using CrudMVCByKING.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudMVCByKING.Controllers
+{
+    public class AuditController : Controller
+
+   
+    {
+        private readonly UsersDbContext _context;
+
+        public AuditController(UsersDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult >Index()
+        {
+            var audits = await _context.AuditTrailRecords.ToListAsync();
+
+            return View(audits);
+
+        }
+
+     
+    }
+}
---
+++ Models/AuditTrailRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudMVCByKING.Models
+{
+    public class AuditTrailRecord
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Action { get; set; }
+        public string? EntityType { get; set; }
+        public string? OldValues { get; set; }
+        public string? NewValues { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
